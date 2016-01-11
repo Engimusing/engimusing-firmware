a758ae0b52bd07eba27a1d2537c8ec9a0acb3fb8
Joe George <joeg@engimusing.com>
2016-01-11 16:28:12 -0700

    fixed file headers for bootloader files

--- cortex-m0plus/1.0.1/bootloaders/EFM32ZGUSB/rtc.c
+++ cortex-m0plus/1.0.1/bootloaders/EFM32ZGUSB/rtc.c
@@ -1,11 +1,11 @@
 /**************************************************************************
  * @file rtc.c
- * @brief EFM32 Bootloader.
- * @author Engimusing LLC
- * @version 1.0
+ * @brief Boot Loader
+ * @author Silicon Labs
+ * @version 1.68
  ******************************************************************************
  * @section License
- * <b>(C) Copyright 2014 Engimusing LLC, http://www.engimusing.com</b>
+ * <b>(C) Copyright 2014 Silicon Labs, http://www.silabs.com</b>
  *******************************************************************************
  *
  * Permission is granted to anyone to use this software for any purpose,
@@ -30,6 +30,26 @@
  * arising from your use of this Software.
  *
  ******************************************************************************/
+
+//*****************************************************************************
+// Copyright (c) 2015-2016 Engimusing LLC.  All right reserved.
+//
+// Modifications by Engimusing are released as a library
+// that is free software; you can redistribute it and/or
+// modify it under the terms of the GNU Lesser General Public
+// License as published by the Free Software Foundation; either
+// version 2.1 of the License, or (at your option) any later version.
+//
+// This library is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+// Lesser General Public License for more details.
+//
+// You should have received a copy of the GNU Lesser General Public
+// License along with this library; if not, write to the Free Software
+// Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
+// 
+//*****************************************************************************
 
 
 #include "config.h"
