4adf1f5c67eb5768e9108555121c12d0c61c946b
Joe George <joeg@engimusing.com>
2016-02-17 14:02:21 -0700

    in midst of changes to get polling working

--- cortex-m0plus/1.0.1/libraries/EFM32ZGUSB/webapp/efmzgusb.py
+++ cortex-m0plus/1.0.1/libraries/EFM32ZGUSB/webapp/efmzgusb.py
@@ -1,50 +1,14 @@
+#!/usr/bin/env python
+
 import os, os.path
 import string
 import serial
 import time
 import curses.ascii
 import json
-
 import cherrypy
+import EFMcomm as efm
 
-def get_xml_line(sport, str):
-        sport.write(str)
-        cnt = 0
-        timeout = 5
-        str = ""
-        mustend = time.time() + timeout
-        while time.time() < mustend:
-            if sport.inWaiting() > 0:
-                c = sport.read()
-                if cnt > 1 and c == '\n':
-                    break
-                if curses.ascii.isgraph(c) != 0:
-                    cnt = cnt + 1
-                    str = ''.join((str, c))
-        if cnt < 1:
-            if time.time() > mustend:
-                print "timed out waiting for line"
-                str = "time out waiting for response"
-            else:
-                print "some other error"
-                str = "response error"
-        return str
-
-def getSerialPort():
-    s = serial.Serial(port="/dev/ttyUSB0",
-        baudrate=115200, 
-        bytesize=8,
-        parity='N',
-        stopbits=1,
-        timeout=5,
-        xonxoff=0,
-        rtscts=0)
-    if(s.isOpen() == False):
-        s.open()
-
-    if(s.isOpen() == True):
-        print "----->>> /dev/ttyUSB0 Open"
-    return s
 
 class App(object):
     @cherrypy.expose
@@ -54,13 +18,26 @@ class App(object):
     @cherrypy.expose
     @cherrypy.tools.json_out()
     def getBoardInfo(self):
-        s = getSerialPort()
         print "----->>> get board name called"
-        xml = {}
-        xml = get_xml_line(s,"{\"MOD\":\"EFMUSB\", \"TYP\":\"BRDINFO\"}")
-        print "------>>>> " + xml
-        return json.loads(xml)
+        brdinfo = efm.sendCommand("{\"MOD\":\"EFMUSB\", \"TYP\":\"BRDINFO\"}")
+        print "------>>>> " + brdinfo + " " + str(len(brdinfo))
+        return json.loads(brdinfo)
 
+    @cherrypy.expose
+    @cherrypy.tools.json_out()
+    def getTempVDD(self):
+        print "----->>> get temp vdd called"
+        tempVDD = efm.sendCommand("{\"MOD\":\"EFMUSB\", \"TYP\":\"TEMPVDD\"}")
+        print "------>>>> " + tempVDD + " " + str(len(tempVDD))
+        return json.loads(tempVDD)
+
+    @cherrypy.expose
+    @cherrypy.tools.json_out()
+    def getLEDstates(self):
+        print "----->>> get LED states called"
+        ledstates = efm.sendCommand("{\"MOD\":\"EFMUSB\", \"TYP\":\"LEDR\", \"IID\":\"ALL\"}")
+        print "------>>>> " + ledstates + " " + str(len(ledstates))
+        return json.loads(ledstates)
 
 if __name__ == '__main__':
     print "----->>> Server Started"
@@ -78,11 +55,3 @@ if __name__ == '__main__':
 
     cherrypy.quickstart(App(), '/', conf)
 
-"""
-[/]
-tools.staticdir.root = "/home/site"
-
-[/style.css]
-tools.staticfile.on = True
-tools.staticfile.filename = "/home/site/style.css"
-"""
--- cortex-m0plus/1.0.1/libraries/EFM32ZGUSB/webapp/scripts/efmzgusb.js
+++ cortex-m0plus/1.0.1/libraries/EFM32ZGUSB/webapp/scripts/efmzgusb.js
@@ -1,4 +1,5 @@
 $(document).ready(function() {
+
     var request = $.ajax({
         'url': '/getBoardInfo'
     });
@@ -14,4 +15,31 @@ $(document).ready(function() {
     request.fail(function(jqXHR, textStatus) {
         alert('Request failed: ' + textStatus);
     });
+    setInterval(function() { 
+	var request1 = $.ajax({
+	    'url': '/getTempVDD'
+	});
+	request1.done(function(response) {
+	    $('#vdd').text(response.upVDD);
+	    $('#tempc').text(response.CPUTEMPC);
+	    $('#tempf').text(response.CPUTEMPF);
+	});
+	request1.fail(function(jqXHR, textStatus) {
+	    alert('Request1 failed: ' + textStatus);
+	});
+
+	var request2 = $.ajax({
+	    'url': '/getLEDstates'
+	});
+	request2.done(function(response) {
+	    $('#redled').text(response.REDLED);
+	    $('#greenled').text(response.GREENLED);
+	    $('#blueled').text(response.BLUELED);
+	});
+	request2.fail(function(jqXHR, textStatus) {
+	    alert('Request2 failed: ' + textStatus);
+	});
+    }, 1000);
 });
+
+
