11375059c322d334db7f06ba76706c99eb6c795c
Joe George <joeg@engimusing.com>
2016-06-16 18:22:09 -0600

    bug fixes

--- cortex-m0plus/1.0.1/variants/EFM32TG110/pins_arduino.c
+++ cortex-m0plus/1.0.1/variants/EFM32TG110/pins_arduino.c
@@ -21,58 +21,58 @@
 
 // ----------------- Pin and Port Arrays -----------------------------------------------------------------------
 
-//                                      X1    X2    X3    X4    X5    X6    X7    SWO   LED   SDA   SCL
-//                                       5     6    17    18    23    24     1     21    8     13    14
-const uint8_t dPorts[]         = {0, 0,PORTB,PORTB,PORTC,PORTC,PORTE,PORTE,PORTA,PORTF,PORTE,PORTD,PORTD};
-const uint8_t dPins[]          = {0, 0,  7,    8,   14,   15,   12,   13,    0,    2,   13,    6,    7  };
+//                                       2     3     4     5     6     7     8      9    10    11    12    13
+//                                      X1    X2    X3    X4    X5    X6    X7    SWO   LED   SDA   SCL   LED
+//                                       5     6    17    18    23    24     1     21    8     13    14    8
+const uint8_t dPorts[]         = {0, 0,PORTB,PORTB,PORTC,PORTC,PORTE,PORTE,PORTA,PORTF,PORTB,PORTD,PORTD,PORTB};
+const uint8_t dPins[]          = {0, 0,  7,    8,   14,   15,   12,   13,    0,    2,   11,    6,    7,    11};
 
 // GPIO Interrupt Pins                  X1    X2    X3    X4    X5    X6    X7    SWO   LED   SDA   SCL
-const uint8_t iPorts[13]       = {0,0,PORTB,PORTB,PORTC,PORTC,PORTE,PORTE,PORTA,PORTF,PORTE,PORTD,PORTD};
-const uint8_t iPins[13]        = {0,0,   7,    8,   14,   15,   12,   13,    0,    2,   13,    6,    7  };
+const uint8_t iPorts[]         = {0,0,PORTB,PORTB,PORTC,PORTC,PORTE,PORTE,PORTA,PORTF,PORTE,PORTD,PORTD,0};
+const uint8_t iPins[]          = {0,0,   7,    8,   14,   15,   12,   13,    0,    2,   13,    6,    7 ,0};
 
 // ADC Pins
-const uint8_t adcPorts[13]     = {0,0,   0,    0,    0,    0,    0,    0,    0,    0,    0, PORTD,PORTD  };
-const uint8_t adcPins[13]      = {0,0,   0,    0,    0,    0,    0,    0,    0,    0,    0,    6,    7  };
+const uint8_t adcPorts[]       = {0,0,   0,    0,    0,    0,    0,    0,    0,    0,    0, PORTD,PORTD,0};
+const uint8_t adcPins[]        = {0,0,   0,    0,    0,    0,    0,    0,    0,    0,    0,    6,    7 ,0};
 
 // ACMP Pins
-const uint8_t acmpPorts[13]    = {0,0,   0,    0, PORTC,PORTC,   0,    0,    0,    0,    0,    0,     0};
-const uint8_t acmpPins[13]     = {0,0,   0,    0,   14,   15,    0,    0,    0,    0,    0,    0,     0};
+const uint8_t acmpPorts[]      = {0,0,   0,    0, PORTC,PORTC,   0,    0,    0,    0,    0,    0,     0,0};
+const uint8_t acmpPins[]       = {0,0,   0,    0,   14,   15,    0,    0,    0,    0,    0,    0,     0,0};
 
 // DAC Pins
 //                                       5     6    17    18    23    24     1     21    8     13    14
-const uint8_t dacPorts[13]     = {0,0,   0,    0, PORTC,PORTC,   0,    0,    0,    0,    0,     0,    0};
-const uint8_t dacPins[13]      = {0,0,   0,    0,   14,   15,    0,    0,    0,    0,    0,     0,    0};
+const uint8_t dacPorts[]       = {0,0,   0,    0, PORTC,PORTC,   0,    0,    0,    0,    0,     0,    0,0};
+const uint8_t dacPins[]        = {0,0,   0,    0,   14,   15,    0,    0,    0,    0,    0,     0,    0,0};
 
 //                                     X1    X2     X3    X4    X5    X6    X7    SWO   LED  SDA  SCL
 //                                      5     6     17    18    23    24     1     21    8    13   14
-const uint8_t timerPorts[13]   = {0,0,PORTB,PORTB,PORTC,PORTC,PORTE,PORTE,PORTA,PORTF,   0,PORTD,PORTD};
-const uint8_t timerPins[13]    = {0,0,  7,    8,    14,   15,   12,   13,    0,    2,    0,   6,    7  };
+const uint8_t timerPorts[]   = {0,0,PORTB,PORTB,PORTC,PORTC,PORTE,PORTE,PORTA,PORTF,   0,PORTD,PORTD,0};
+const uint8_t timerPins[]    = {0,0,  7,    8,    14,   15,   12,   13,    0,    2,    0,   6,    7 ,0};
 
-const uint8_t timerPinMap[13]  = {9,9,  1,    1,    1,    1,    1,    9,     0,    0,    9,   1,    1};
-const uint8_t timerCCindex[13] = {9,9,  0,    1,    1,    2,    2,    9,     0,    2,    9,   0,    1};
+const uint8_t timerPinMap[]  = {9,9,  1,    1,    1,    1,    1,    9,     0,    0,    9,   1,    1, 0,0};
+const uint8_t timerCCindex[] = {9,9,  0,    1,    1,    2,    2,    9,     0,    2,    9,   0,    1, 0,0};
 
-const uint32_t timerRoutes[13] = {0, // 0
-				  0, // 1
-				  TIMER_ROUTE_LOCATION_LOC3 | TIMER_ROUTE_CC0PEN, // 2: PB7  - TIM1_CC0 #3
-				  TIMER_ROUTE_LOCATION_LOC3 | TIMER_ROUTE_CC1PEN, // 3: PB8  - TIM1_CC1 #3
-				  TIMER_ROUTE_LOCATION_LOC0 | TIMER_ROUTE_CC1PEN, // 4: PC14 - TIM1_CC1 #0
-				  TIMER_ROUTE_LOCATION_LOC0 | TIMER_ROUTE_CC2PEN, // 5: PC15 - TIM1_CC2 #0
-				  TIMER_ROUTE_LOCATION_LOC1 | TIMER_ROUTE_CC2PEN, // 6: PE12 - TIM1_CC2 #1
-				  0,
-				  TIMER_ROUTE_LOCATION_LOC0 | TIMER_ROUTE_CC0PEN, // 8: PA0  - TIM0_CC0 #0
-				  TIMER_ROUTE_LOCATION_LOC5 | TIMER_ROUTE_CC2PEN, // 9: PF2  - TIM0_CC2 #5
-				  0, //10
-				  TIMER_ROUTE_LOCATION_LOC4 | TIMER_ROUTE_CC0PEN, //11: PD6  - TIM1_CC0 #4
-				  TIMER_ROUTE_LOCATION_LOC4 | TIMER_ROUTE_CC1PEN};//12: PD7  - TIM1_CC1 #4
-
+const uint32_t timerRoutes[] = {0, // 0
+				0, // 1
+				TIMER_ROUTE_LOCATION_LOC3 | TIMER_ROUTE_CC0PEN, // 2: PB7  - TIM1_CC0 #3
+				TIMER_ROUTE_LOCATION_LOC3 | TIMER_ROUTE_CC1PEN, // 3: PB8  - TIM1_CC1 #3
+				TIMER_ROUTE_LOCATION_LOC0 | TIMER_ROUTE_CC1PEN, // 4: PC14 - TIM1_CC1 #0
+				TIMER_ROUTE_LOCATION_LOC0 | TIMER_ROUTE_CC2PEN, // 5: PC15 - TIM1_CC2 #0
+				TIMER_ROUTE_LOCATION_LOC1 | TIMER_ROUTE_CC2PEN, // 6: PE12 - TIM1_CC2 #1
+				0,
+				TIMER_ROUTE_LOCATION_LOC0 | TIMER_ROUTE_CC0PEN, // 8: PA0  - TIM0_CC0 #0
+				TIMER_ROUTE_LOCATION_LOC5 | TIMER_ROUTE_CC2PEN, // 9: PF2  - TIM0_CC2 #5
+				0, //10
+				TIMER_ROUTE_LOCATION_LOC4 | TIMER_ROUTE_CC0PEN, //11: PD6  - TIM1_CC0 #4
+				TIMER_ROUTE_LOCATION_LOC4 | TIMER_ROUTE_CC1PEN,  //12: PD7  - TIM1_CC1 #4
+				0};
 
 // ------------------------------------------------------------------------------------------------------------
 
 // This function verifies the pin is valid for this variant
-// 1 5 6 8 13 14 17 18 21 23 24
+// 2-13
 uint8_t valid_pin(uint8_t pin) {
-  if((pin < 1) || ((pin >= 2) && (pin <= 4)) || (pin == 7) || ((pin >= 9) && (pin <= 12))
-     || (pin == 15) || (pin == 16) || (pin == 19) || (pin == 20) || (pin == 22) || (pin > 24)) {
+  if((pin < 2) || (pin > 13)) {
     return 0;
   } else {
     return 1;
--- cortex-m0plus/1.0.1/variants/EFM32ZG108/pins_arduino.c
+++ cortex-m0plus/1.0.1/variants/EFM32ZG108/pins_arduino.c
@@ -21,36 +21,37 @@
 
 // ----------------- Pin and Port Arrays -----------------------------------------------------------------------
 
-//                                      X1    X2    X3    X4    X5    X6    X7    SWO   LED   SDA   SCL
-//                                       5     6    17    18    23    24     1     21    8     13    14
-const uint8_t dPorts[]         = {0, 0,PORTB,PORTB,PORTC,PORTC,PORTE,PORTE,PORTA,PORTF,PORTE,PORTD,PORTD};
-const uint8_t dPins[]          = {0, 0,  7,    8,   14,   15,   12,   13,    0,    2,   13,    6,    7  };
+//                                       2     3     4     5     6     7     8      9    10    11    12    13
+//                                      X1    X2    X3    X4    X5    X6    X7    SWO   LED   SDA   SCL   LED
+//                                       5     6    17    18    23    24     1     21    8     13    14     8
+const uint8_t dPorts[]         = {0, 0,PORTB,PORTB,PORTC,PORTC,PORTE,PORTE,PORTA,PORTF,PORTB,PORTD,PORTD,PORTB};
+const uint8_t dPins[]          = {0, 0,  7,    8,   14,   15,   12,   13,    0,    2,   11,    6,    7,   11  };
 
 // GPIO Interrupt Pins                  X1    X2    X3    X4    X5    X6    X7    SWO   LED   SDA   SCL
-const uint8_t iPorts[13]       = {0,0,PORTB,PORTB,PORTC,PORTC,PORTE,PORTE,PORTA,PORTF,PORTE,PORTD,PORTD};
-const uint8_t iPins[13]        = {0,0,   7,    8,   14,   15,   12,   13,    0,    2,   13,    6,    7  };
+const uint8_t iPorts[]       = {0,0,PORTB,PORTB,PORTC,PORTC,PORTE,PORTE,PORTA,PORTF,PORTE,PORTD,PORTD,0};
+const uint8_t iPins[]        = {0,0,   7,    8,   14,   15,   12,   13,    0,    2,   13,    6,    7 ,0};
 
 // ADC Pins
-const uint8_t adcPorts[13]       = {0};
-const uint8_t adcPins[13]        = {0};
+const uint8_t adcPorts[14]       = {0};
+const uint8_t adcPins[14]        = {0};
 
 // ACMP Pins
-const uint8_t acmpPorts[13] = {0};
-const uint8_t acmpPins[13]  = {0};
+const uint8_t acmpPorts[14] = {0};
+const uint8_t acmpPins[14]  = {0};
 
 // DAC Pins
-const uint8_t dacPorts[11] = {0};
-const uint8_t dacPins[11]  = {0};
+const uint8_t dacPorts[14] = {0};
+const uint8_t dacPins[14]  = {0};
 
 //                                     X1    X2     X3    X4    X5    X6    X7    SWO   LED  SDA  SCL
 //                                      5     6     17    18    23    24     1     21    8    13   14
-const uint8_t timerPorts[13]   = {0,0,PORTB,PORTB,PORTC,PORTC,PORTE,PORTE,PORTA,PORTF,   0,PORTD,PORTD};
-const uint8_t timerPins[13]    = {0,0,  7,    8,    14,   15,   12,   13,    0,    2,    0,   6,    7  };
+const uint8_t timerPorts[14]   = {0,0,PORTB,PORTB,PORTC,PORTC,PORTE,PORTE,PORTA,PORTF,   0,PORTD,PORTD,0};
+const uint8_t timerPins[14]    = {0,0,  7,    8,    14,   15,   12,   13,    0,    2,    0,   6,    7 ,0};
 
-const uint8_t timerPinMap[13]  = {9,9,  1,    1,    1,    1,    1,    9,     0,    0,    9,   1,    1};
-const uint8_t timerCCindex[13] = {9,9,  0,    1,    1,    2,    2,    9,     0,    2,    9,   0,    1};
+const uint8_t timerPinMap[14]  = {9,9,  1,    1,    1,    1,    1,    9,     0,    0,    9,   1,    1, 0};
+const uint8_t timerCCindex[14] = {9,9,  0,    1,    1,    2,    2,    9,     0,    2,    9,   0,    1, 0};
 
-const uint32_t timerRoutes[13] = {0, // 0
+const uint32_t timerRoutes[14] = {0, // 0
 				  0, // 1
 				  TIMER_ROUTE_LOCATION_LOC3 | TIMER_ROUTE_CC0PEN, // 2: PB7  - TIM1_CC0 #3
 				  TIMER_ROUTE_LOCATION_LOC3 | TIMER_ROUTE_CC1PEN, // 3: PB8  - TIM1_CC1 #3
@@ -62,16 +63,15 @@ const uint32_t timerRoutes[13] = {0, // 0
 				  TIMER_ROUTE_LOCATION_LOC5 | TIMER_ROUTE_CC2PEN, // 9: PF2  - TIM0_CC2 #5
 				  0, //10
 				  TIMER_ROUTE_LOCATION_LOC4 | TIMER_ROUTE_CC0PEN, //11: PD6  - TIM1_CC0 #4
-				  TIMER_ROUTE_LOCATION_LOC4 | TIMER_ROUTE_CC1PEN};//12: PD7  - TIM1_CC1 #4
-
+				  TIMER_ROUTE_LOCATION_LOC4 | TIMER_ROUTE_CC1PEN, //12: PD7  - TIM1_CC1 #4
+				  0};
 
 // ------------------------------------------------------------------------------------------------------------
 
 // This function verifies the pin is valid for this variant
-// 1 5 6 8 13 14 17 18 21 23 24
+// 2-13
 uint8_t valid_pin(uint8_t pin) {
-  if((pin < 1) || ((pin >= 2) && (pin <= 4)) || (pin == 7) || ((pin >= 9) && (pin <= 12))
-     || (pin == 15) || (pin == 16) || (pin == 19) || (pin == 20) || (pin == 22) || (pin > 24)) {
+  if((pin < 2) || (pin > 13)) {
     return 0;
   } else {
     return 1;
