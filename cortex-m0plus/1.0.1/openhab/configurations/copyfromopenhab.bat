e951394c81f9fdccc4ec6482f4d8d64a2e8a46bb
Tim George <github@tdgeorge.com>
2016-08-12 15:21:28 -0600

    Initial checkin of the openHAB configuration. Has support for LED's and Switches

---
+++ cortex-m0plus/1.0.1/openhab/configurations/copyfromopenhab.bat
@@ -0,0 +1,2 @@
+xcopy D:\emus2016\openHAB\configurations\* C:\Users\TDGeorge\AppData\Local\Arduino15\packages\engimusing\hardware\cortex-m0plus\1.0.1\openhab\configurations\ /y /e
+pause
---
+++ cortex-m0plus/1.0.1/openhab/copytoopenhab.bat
@@ -0,0 +1,2 @@
+xcopy C:\Users\TDGeorge\AppData\Local\Arduino15\packages\engimusing\hardware\cortex-m0plus\1.0.1\openhab\configurations\* D:\emus2016\openHAB\configurations\ /y /e
+pause
