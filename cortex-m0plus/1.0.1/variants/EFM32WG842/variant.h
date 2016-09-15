2e9b600cca7e1127826f1358aba73f9299194336
Tim George <github@tdgeorge.com>
2016-09-14 18:11:23 -0600

    Fixed the ADC setup for the WG842

--- cortex-m0plus/1.0.1/variants/EFM32WG842/variant.h
+++ cortex-m0plus/1.0.1/variants/EFM32WG842/variant.h
@@ -43,6 +43,16 @@ static const uint8_t A7  = 35;
 #define ADC_RESOLUTION		12
 
 
+// PWM Resolution
+
+#define PWM_INTERFACE        PWM
+#define PWM_INTERFACE_ID     ID_PWM
+#define PWM_FREQUENCY	     1000
+#define PWM_MAX_DUTY_CYCLE   255
+#define PWM_MIN_DUTY_CYCLE   0
+#define PWM_RESOLUTION	     16
+
+
 // Wire Interfaces (I2C)
 
 #define WIRE_INTERFACES_COUNT 1
