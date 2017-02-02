01e4b585467bbf54053a4015480a2a6496228769
Joe George <joeg@engimusing.com>
2017-02-02 14:16:05 -0700

    updated MQTT_Sketch_Tutorial

--- efm32/1.0.0/libraries/MqttHub/examples/MQTT_Sketch_Tutorial/MQTT_Sketch_Tutorial.ino
+++ efm32/1.0.0/libraries/MqttHub/examples/MQTT_Sketch_Tutorial/MQTT_Sketch_Tutorial.ino
@@ -19,43 +19,58 @@
   {"TOP":"EFM/BOARD/LED2/CTL","PLD":"ON"}
   {"TOP":"EFM/BOARD/LED2/CTL","PLD":"OFF"}
   {"TOP":"EFM/BOARD/LED2/CTL","PLD":"STATUS"}
+
+  {"TOP":"EFMUSB/CPU/TMPC"}
+  {"TOP":"EFMUSB/CPU/TMPF"}
+  {"TOP":"EFMUSB/CPU/VDD"}
 */
 
 MqttHub HUB;
 
 MqttSerialPort SerialPort;
 
+// Instantiate classes for each the LEDs on the EFMUSB CPU board
+// The onOffCtlClass allows for turning the LEDs on and off and
+// requesting their status
+
 OnOffCtlModule LEDCtrl0;
 OnOffCtlModule LEDCtrl1;
 OnOffCtlModule LEDCtrl2;
+
+// Instantiate classes for the CPU temperature and supply voltage
+// These classes can be used to read the values or can be set up
+// to periodically report the values
+
+CpuVddModule EFMCPUVDD;
+CpuTempModule EFMCPUTMP;
 
 void setup()
 {
   SerialPort.begin(HUB, Serial);
 
+  // Start each of the LED services
+  // The first parameter is the LED pin
+  // The second parameter is the module name
+  // The third parameter is the on state for the LEDs
   LEDCtrl0.begin(HUB, ledId[0], "EFM/BOARD/LED0", LOW);
   LEDCtrl1.begin(HUB, ledId[1], "EFM/BOARD/LED1", LOW);
   LEDCtrl2.begin(HUB, ledId[2], "EFM/BOARD/LED2", LOW);
+
+  // Start each of the CPU services
+  // The first parameter is the module name
+  // The other parameters are time values in tenths of seconds
+  // The CPUVDD will send a status every 10 seconds
+  EFMCPUVDD.begin(HUB, "EFM/CPU/VDD", 50);
+
+  // The CPUTEMP has two possible status options
+  // The first number is 5 seconds for a Farenheit reading
+  // The second number is 5 seconds for a Celcius reading
+  EFMCPUTMP.begin(HUB, "EFM/CPU/TEMP", 50, 50);
 }
 //int lastMillisOn = 0;
 //int lastMillisOff = 1000;
 
-uint32_t pingTime=0;
 void loop()
 {
   HUB.update();
-   
-  /* //example of how to turn on and off a light using the onOffCtlClass
-     //status of the pin will be sent to the MQTT broker.
-  if(millis() > lastMillisOff + 2000)
-  {
-    LEDCtrl2.setPinState(LOW);
-    lastMillisOff = millis();
-  }
-  if(millis() > lastMillisOn + 2000)
-  {
-    LEDCtrl2.setPinState(HIGH);
-    lastMillisOn = millis();
-  }*/
-
 }
