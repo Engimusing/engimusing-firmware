31511f168e9ded9260ea6c8c130f8f1a0f6d8f6c
Joe George <joeg@engimusing.com>
2017-02-09 14:51:17 -0700

    trivial clean up

--- efm32/1.0.0/variants/EFM32ZG222/pins_arduino.c
+++ efm32/1.0.0/variants/EFM32ZG222/pins_arduino.c
@@ -37,9 +37,9 @@ const uint8_t adcPins[11]   = {0,0,  4,    6,   0, 0, 0, 0, 0, 0,   7  };
 
 const uint32_t adcChannel[] =  {-1,-1,EM_ADC4,EM_ADC6,-1,-1,-1,EM_DAC0,-1,-1, -1, EM_ADC7};
 const ADC_SingleInput_TypeDef adcChannelNum[] =  {adcSingleInpCh0,adcSingleInpCh0,adcSingleInpCh4,
-														   adcSingleInpCh6,adcSingleInpCh0,adcSingleInpCh0,
-														   adcSingleInpCh0,adcSingleInpCh0,adcSingleInpCh0,
-														   adcSingleInpCh0,adcSingleInpCh0, adcSingleInpCh7};
+						  adcSingleInpCh6,adcSingleInpCh0,adcSingleInpCh0,
+						  adcSingleInpCh0,adcSingleInpCh0,adcSingleInpCh0,
+						  adcSingleInpCh0,adcSingleInpCh0, adcSingleInpCh7};
 
 
 
