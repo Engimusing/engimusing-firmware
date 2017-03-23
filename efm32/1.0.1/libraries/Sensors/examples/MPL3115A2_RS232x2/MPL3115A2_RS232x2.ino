bd9b44d2b4d1aeb50f5bc7954498d87cca03fb07
tdgeorge <github@tdgeorge.com>
2017-03-22 20:07:24 -0600

    Minor change to copyright year.

--- efm32/1.0.1/libraries/Sensors/examples/MPL3115A2_RS232x2/MPL3115A2_RS232x2.ino
+++ efm32/1.0.1/libraries/Sensors/examples/MPL3115A2_RS232x2/MPL3115A2_RS232x2.ino
@@ -1,4 +1,5 @@
 /*
+  Copyright (c) 2016-2017 Engimusing LLC.  All right reserved.
 
   This library is free software; you can redistribute it and/or
   modify it under the terms of the GNU Lesser General Public
