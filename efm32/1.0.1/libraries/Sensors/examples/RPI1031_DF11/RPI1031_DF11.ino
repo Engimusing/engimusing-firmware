ecef1cce16e13ea46e5637732494937f7076a900
tdgeorge <github@tdgeorge.com>
2017-04-10 16:13:46 -0600

    Adding RPI1031 device and examples.

---
+++ efm32/1.0.1/libraries/Sensors/examples/RPI1031_DF11/RPI1031_DF11.ino
@@ -0,0 +1,97 @@
+/*
+  Copyright (c) 2016-2017 Engimusing LLC.  All right reserved.
+  
+  This library is free software; you can redistribute it and/or
+  modify it under the terms of the GNU Lesser General Public
+  License as published by the Free Software Foundation; either
+  version 2.1 of the License, or (at your option) any later version.
+
+  This library is distributed in the hope that it will be useful,
+  but WITHOUT ANY WARRANTY; without even the implied warranty of
+  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+  See the GNU Lesser General Public License for more details.
+
+  You should have received a copy of the GNU Lesser General Public
+  License along with this library; if not, write to the Free Software
+  Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
+*/
+/* Example for how to print out readings from the RPI1031  DF11 board using the EFM32ZGUSB Engimusing board
+    There are 2 devices on this board. An LED and a RPI1031 Tilt sensor.
+    See https://www.engimusing.com/products/rpi1031-1 for more information about the board.
+*/
+
+#if !defined(EFM32ZGUSB)
+#error Incorrect Board Selected! Please select Engimusing EFM32ZGUSB from the Tools->Board: menu.
+#endif
+
+#include <RPI1031Device.h>
+
+RPI1031Device RPI1031;
+
+void setup()
+{
+  Serial.begin(115200);
+
+  pinMode(LED_BUILTIN, OUTPUT);
+  Serial.println("Simple RPI1031 example 0");
+
+  
+  //sets up the 5 pins needed to setup and communicate with the RPI1031
+  RPI1031.begin(3,4,5,6,7,10);
+
+}
+
+int lastMillis = 0; // store the last time the current was printed.
+int printDelay = 1000; //print every second.
+
+void loop()
+{
+
+  static int on = HIGH;
+
+  RPI1031.update();
+  
+
+  if(millis() - lastMillis > printDelay)
+  {
+    lastMillis = millis();
+
+    digitalWrite(LED_BUILTIN, on);   // toggle the LED (HIGH is the voltage level)
+    
+    bool switchState[2];
+    bool risingEdge[2];
+    bool fallingEdge[2];
+      
+    for(int i = 0; i < 2; i++)
+    {
+      switchState[i] = RPI1031.switchState(i);
+      risingEdge[i] = RPI1031.risingEdge(i);
+      fallingEdge[i] = RPI1031.fallingEdge(i);
+      
+      Serial.print("Switch ");
+      Serial.print(i + 1);
+        
+      if(switchState[i])
+      {
+        
+        Serial.println(" state = on");
+      }
+      else
+      {
+        Serial.println(" state = off");
+      }
+      
+      if(risingEdge[i])
+      {
+        Serial.println("Rising Edge");
+      }
+      
+      if(fallingEdge[i])
+      {
+        Serial.println("Falling Edge");
+      }
+    }
+
+    on = (on) ? LOW : HIGH;  // on alternates between LOW and HIGH
+  }
+}
---
+++ efm32/1.0.1/libraries/Sensors/examples/RPI1031_DF11_MQTT/RPI1031_DF11_MQTT.ino
@@ -0,0 +1,77 @@
+/*
+  Copyright (c) 2016-2017 Engimusing LLC.  All right reserved.
+  
+  This library is free software; you can redistribute it and/or
+  modify it under the terms of the GNU Lesser General Public
+  License as published by the Free Software Foundation; either
+  version 2.1 of the License, or (at your option) any later version.
+
+  This library is distributed in the hope that it will be useful,
+  but WITHOUT ANY WARRANTY; without even the implied warranty of
+  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+  See the GNU Lesser General Public License for more details.
+
+  You should have received a copy of the GNU Lesser General Public
+  License along with this library; if not, write to the Free Software
+  Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
+*/
+/* Example for how to setup the MQTT client for the RPI1031 DF11 board using the EFM32ZGUSB Engimusing board
+    There are 2 devices on this board. An LED and a RPI1031 Tilt sensor.
+    See https://www.engimusing.com/products/rpi1031-1 for more information about the board.
+*/
+
+#if !defined(EFM32ZGUSB)
+#error Incorrect Board Selected! Please select Engimusing EFM32ZGUSB from the Tools->Board: menu.
+#endif
+
+//Include the MqttModule to get the MQTT client classes
+#include <MqttHub.h>
+#include <MqttPort.h>
+#include <MqttModule.h>
+
+#include <RPI1031Device.h>
+
+
+/*
+  EFM32ZGUSB Commands:
+  {"TOP":"EFM32ZGUSB/BOARD/LED/CTL","PLD":"ON"}
+  {"TOP":"EFM32ZGUSB/BOARD/LED/CTL","PLD":"OFF"}
+  {"TOP":"EFM32ZGUSB/BOARD/LED/CTL","PLD":"STATUS"}
+
+  {"TOP":"EFM32ZGUSB/BOARD/RPI1031/","PLD":"STATUS"}
+*/
+
+MqttHub HUB;
+MqttSerialPort serialPort;
+
+//MQTT class defintions
+// The MqttModule classes are automatically registered with the COMM
+// object when begin() is called so they can be updated
+// whenever HUB.update() is called.
+OnOffCtlModule LEDCtrl;
+
+RPI1031Device RPI1031;
+SimpleMqttModule RPI1031MqttMod;
+
+void setup() 
+{
+  serialPort.begin(HUB, Serial);
+
+  //Initialize the on off control to connect it to
+  // the LED that is on the board
+  LEDCtrl.begin(HUB, 13, "EFM32ZGUSB/BOARD/LED", HIGH);
+
+  
+  //sets up the 5 pins needed to setup and communicate with the RPI1031
+  RPI1031.begin(3,4,5,6,7,10);
+  RPI1031MqttMod.begin(HUB, RPI1031, "EFM32ZGUSB/BOARD/RPI1031", 10000);
+}
+
+void loop() 
+{
+
+  //Update the MQTT communication so it
+  // can send statuses and recieve requests
+  HUB.update();
+
+}
