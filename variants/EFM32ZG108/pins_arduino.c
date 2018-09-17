037fd43d9ba1304ae8bfdad14136ef3b036d72a5
Joe George <joeg@engimusing.com>
2018-09-17 16:51:50 -0600

    allowed debug pins to be used as gpio on zg108

--- variants/EFM32ZG108/pins_arduino.c
+++ variants/EFM32ZG108/pins_arduino.c
@@ -24,11 +24,11 @@
 
 // ----------------- Pin and Port Arrays -----------------------------------------------------------------------
 
-//                                       2     3     4     5     6     7     8      9    10    11    12    13    14
-//                                      X1    X2    X3    X4    X5    X6    X7    SWO   LED   SDA   SCL   LED    
-//                                       5     6    17    18    23    24     1     21    8     13    14     8     1
+//                                       2     3     4     5     6     7     8      9   10     11    12    13    14     15     16
+//                                      X1    X2    X3    X4    X5    X6    X7    SWO  LED    SDA   SCL   LED    
+//                                       5     6    17    18    23    24     1     21    8     13    14     8     1     19     20
 const uint8_t dPorts[]         = {0, 0,PORTB,PORTB,PORTC,PORTC,PORTE,PORTE,PORTA,PORTF,PORTB,PORTD,PORTD,PORTB, PORTA, PORTF, PORTF};
-const uint8_t dPins[]          = {0, 0,  7,    8,   14,   15,   12,   13,    0,    2,   11,    6,    7,   11,     0, 0, 1  };
+const uint8_t dPins[]          = {0, 0,  7,    8,   14,   15,   12,   13,    0,    2,   11,    6,    7,    11,    0,     0,     1  };
 
 // GPIO Interrupt Pins                  X1    X2    X3    X4    X5    X6    X7    SWO   LED   SDA   SCL
 const uint8_t iPorts[]         = {0,0,PORTB,PORTB,PORTC,PORTC,PORTE,PORTE,PORTA,PORTF,PORTE,PORTD,PORTD,0,PORTA};
@@ -84,7 +84,7 @@ const uint8_t debugPinId[3] = {15,16,9}; //SWCLK, SWDIO, SWO
 // This function verifies the pin is valid for this variant
 // 2-13
 uint8_t valid_pin(uint8_t pin) {
-  if( (pin > 14)) {
+  if( (pin > 16)) {
     return 0;
   } else {
     return 1;
